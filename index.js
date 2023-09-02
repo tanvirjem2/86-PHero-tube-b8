f90ffca3d3b9afd142cec1cead2aa81d338a7352
tanvirjem2 <tanviranjum046@gmail.com>
2023-09-02 16:33:03 +0600

    Finally All doe

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let fetchData = [];
+
 const loadButton = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`)
     const data = await res.json()
@@ -23,9 +24,61 @@ function sortByViews() {
         return bValue - aValue
     })
 
-    loadData(fetchData)
-
     console.log(fetchData);
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = '';
+
+    const cardContainer2 = document.getElementById('card-container-2');
+    cardContainer2.textContent = '';
+
+    if (fetchData.length !== 0) {
+        fetchData.forEach(element => {
+
+            // convert seconds to hrs, min, sec
+            const inputSeconds = parseInt(element.others.posted_date);
+            const time = convertSecondsToTime(inputSeconds);
+
+            // console.log(typeof inputSeconds);
+            const div = document.createElement('div');
+            div.classList.add('mx-auto');
+
+            div.innerHTML = `
+                <div class="relative">
+                    <img class="mb-5 rounded-lg w-[312px] h-[200px]" src=${element.thumbnail} alt="">
+                    <div class="absolute bottom-0 right-0 text-white pr-3 pb-2">
+                        <span class="border-solid border-black border rounded-md bg-[#171717] text-xs p-1">
+                        ${time.hours ? time.hours : '0'} hrs ${time.minutes ? time.minutes : '0'} min ${time.seconds ? time.seconds : '0'} sec</span>
+                    </div>
+                </div>
+                <div class="flex gap-4 mt-5">
+                    <div>
+                        <img class="rounded-full w-10 h-10" src=${element.authors[0].profile_picture} alt="">
+                    </div>
+                    <div class="space-y-2">
+                        <p class="font-bold">${element.title}</p>
+                        <div class="flex items-center gap-3">
+                            <p class="text-sm">${element.authors[0].profile_name}</p>
+                            ${element.authors[0].verified ? '<img src="./fi_10629607.png" alt=""></img>' : ""}
+                        </div>
+                        <p class="text-sm">${element.others.views} views</p>
+                    </div>
+                `
+            cardContainer.appendChild(div)
+        })
+
+    } else if (fetchData.length === 0) {
+        const div = document.createElement('div');
+        div.classList.add('text-center');
+        div.classList.add('space-y-7')
+        div.innerHTML = `
+                <img class="mx-auto" src="./Icon.png" alt="">
+                <p class="text-3xl font-bold">Oops!! Sorry, There is no <br>
+                    content here</p>
+                    `
+        cardContainer2.appendChild(div)
+    }
+
 }
 
 const loadData = async (categoryId) => {
@@ -76,6 +129,7 @@ const loadData = async (categoryId) => {
                 `
             cardContainer.appendChild(div)
         })
+
     } else if (data.data.length === 0) {
         const div = document.createElement('div');
         div.classList.add('text-center');
